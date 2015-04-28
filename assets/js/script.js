fe7efd5eaf5bd27aa709f4f35a530586c0f6b6f4
Nathan Chong <nkmchong@gmail.com>
2015-04-28 04:53:55 -0700

    Scrolling buttons work, resume page navbar now green, done for big screens!

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var $window = $(window),
     $navbar = $('#navbar-wrapper'),
     navbar_top = $navbar.offset().top - 20,
-    work_top = $('#work').offset().top - 20,
+    work_top = $('#work').offset().top - 100,
     $topbar = $('#topbar'),
     navbar_margin = $navbar.height() + 20,
     $content = $('#content-wrapper'),
@@ -15,6 +15,12 @@ $(document).ready(function() {
         $navbar.toggleClass('sticky', $window.scrollTop() > navbar_top);
         $ghost.toggle($window.scrollTop() > navbar_top);
         $topbar.toggleClass('blue', $window.scrollTop() > work_top);
+    });
+    $('#about-link').click(function() {
+        $('html, body').animate({scrollTop: 0}, 500, 'swing');
+    });
+    $('#work-link').click(function() {
+        $('html, body').animate({scrollTop: $("#work").offset().top - 50}, 500, 'swing');
     });
 });
 
